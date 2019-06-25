33401f6ae8da32118bf82126fcf7e2fb351b0b4a
shubh <psubham94@gmail.com>
2019-06-25 17:58:28 +0530

    Update Loading components with better UI

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,6 +3,7 @@ import './Loading.css';
 
 export default function Loading(){
 	return (
-		<div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+		<div class="loader" style={{position:"relative",left:"400px"}}>Loading...</div>
+		// <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
 	)
 }
